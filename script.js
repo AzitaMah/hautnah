1676808ed2e661d0df5d43b095aecef0eba6eb00
Mahmoudi Azita <@am222>
2025-04-28 23:11:40 +0200

    Add navbar, change background-color, add Logo

--- script.js
+++ script.js
@@ -1,55 +1,65 @@
 const imagePairs = [
-    ['./img/Banner1.jpg', './img/Banner2.jpg'],
-    ['./img/Banner3.jpg', './img/Banner4.jpg']
-  ];
+  ['./img/Banner1.jpg', './img/Banner2.jpg'],
+  ['./img/Banner3.jpg', './img/Banner4.jpg']
+];
+
+let currentIndex = 0;
+
+const leftImg = document.querySelector('.img-left');
+const rightImg = document.querySelector('.img-right');
+
+// Initiale Bilder setzen, aber sicherstellen, dass sie sichtbar sind
+leftImg.src = imagePairs[0][0]; // Banner1.jpg
+rightImg.src = imagePairs[0][1]; // Banner2.jpg
+
+// Bilder sofort sichtbar machen, bevor irgendwelche Animationen starten
+leftImg.onload = () => {
+  leftImg.classList.add('active');
+};
+rightImg.onload = () => {
+  rightImg.classList.add('active');
+};
+
+// Funktion zum Zurücksetzen der Bilder
+function resetImages() {
+  leftImg.classList.remove('active', 'fade-out');
+  rightImg.classList.remove('active', 'fade-out');
   
-  let currentIndex = 0;
+  // Reflow erzwingen, damit die Animation neu gestartet werden kann
+  void leftImg.offsetWidth;
+  void rightImg.offsetWidth;
+}
+
+// Funktion für den Bildwechsel
+function nextSlide() {
+  // Zuerst den "fade-out"-Effekt anwenden, damit die Bilder verschwinden
+  leftImg.classList.add('fade-out');
+  rightImg.classList.add('fade-out');
   
-  const leftImg = document.querySelector('.img-left');
-  const rightImg = document.querySelector('.img-right');
+  // Warten, bis die "fade-out"-Animation abgeschlossen ist (2s)
+  setTimeout(() => {
+    // Bildwechsel
+    currentIndex = (currentIndex + 1) % imagePairs.length;
+    leftImg.src = imagePairs[currentIndex][0];
+    rightImg.src = imagePairs[currentIndex][1];
   
-  // Initiale Bilder setzen, aber sicherstellen, dass sie sichtbar sind
-  leftImg.src = imagePairs[0][0]; // Banner1.jpg
-  rightImg.src = imagePairs[0][1]; // Banner2.jpg
-  
-  // Bilder sofort sichtbar machen, bevor irgendwelche Animationen starten
-  leftImg.onload = () => {
+    // Bilder zurücksetzen und neue "active"-Klasse hinzufügen
+    resetImages();
     leftImg.classList.add('active');
-  };
-  rightImg.onload = () => {
     rightImg.classList.add('active');
-  };
-  
-  // Funktion zum Zurücksetzen der Bilder
-  function resetImages() {
-    leftImg.classList.remove('active', 'fade-out');
-    rightImg.classList.remove('active', 'fade-out');
-    
-    // Reflow erzwingen, damit die Animation neu gestartet werden kann
-    void leftImg.offsetWidth;
-    void rightImg.offsetWidth;
+  }, 2000); // fade-out dauert 2s
+}
+
+// Alle 8 Sekunden den Bildwechsel auslösen
+setInterval(nextSlide, 8000);
+
+
+// NAV BAR   Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon 
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
-  
-  // Funktion für den Bildwechsel
-  function nextSlide() {
-    // Zuerst den "fade-out"-Effekt anwenden, damit die Bilder verschwinden
-    leftImg.classList.add('fade-out');
-    rightImg.classList.add('fade-out');
-    
-    // Warten, bis die "fade-out"-Animation abgeschlossen ist (2s)
-    setTimeout(() => {
-      // Bildwechsel
-      currentIndex = (currentIndex + 1) % imagePairs.length;
-      leftImg.src = imagePairs[currentIndex][0];
-      rightImg.src = imagePairs[currentIndex][1];
-    
-      // Bilder zurücksetzen und neue "active"-Klasse hinzufügen
-      resetImages();
-      leftImg.classList.add('active');
-      rightImg.classList.add('active');
-    }, 2000); // fade-out dauert 2s
-  }
-  
-  // Alle 8 Sekunden den Bildwechsel auslösen
-  setInterval(nextSlide, 8000);
-  
+} 
